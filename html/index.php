1994e3954f098e1f3ffb53347ba26650acf226c1
Kev <git@kjs1982.me.uk>
2013-02-24 14:43:22 +0000

    First Leaflet page

--- html/index.php
+++ html/index.php
@@ -1,1 +1,29 @@
-<h1>Hello World</h1>
+<!DOCTYPE html>
+	<html lang="en-GB"> 
+	<head>
+		<title>kjs :: Leaftlet</title>
+		<meta name="geo.placename" content="Nottinghamshire, United Kingdom" />
+		<meta name="geo.position" content="52.931373; -1.125981" />
+		<meta name="geo.region" content="GB-NTT" />
+		<meta name="ICBM" content="52.931373, -1.125981" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		
+		 <link rel="stylesheet" href="/assets/leaflet/leaflet.css" />
+		 <!--[if lte IE 8]>
+			 <link rel="stylesheet" href="/assets/leaflet/leaflet.ie.css" />
+		 <![endif]-->
+		<script src="/assets/leaflet/leaflet.js"></script>
+		<style type="text/css">
+			#map { height: 180px; }
+		</style>
+		
+		<script type="text/javascript">
+		// <![CDATA[
+			var map = L.map('map').setView([52.931373, -1.125981], 13);
+		// ]]>
+	</head>
+	<body>
+	<div id="map"></div>
+
+ </body>
+</html>
